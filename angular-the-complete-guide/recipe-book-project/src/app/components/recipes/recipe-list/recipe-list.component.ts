accab674c05091f0b18325aa749e22b4ac0ce873
André Pereira <andrepereira97@yahoo.com>
2022-06-12 15:07:19 -0300

    :sparkles: add recipe details

--- angular-the-complete-guide/recipe-book-project/src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ angular-the-complete-guide/recipe-book-project/src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,21 +6,23 @@ import { Recipe } from '../recipe.model';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe(
-      'any_name',
-      'any_description',
+      'any name',
+      'any description',
       'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
     ),
     new Recipe(
-      'any_name',
-      'any_description',
+      'another name',
+      'another description',
       'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
     ),
   ];
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
