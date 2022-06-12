f4eeaed39971027463acab3a8b196779afdb2d0e
André Pereira <andrepereira97@yahoo.com>
2022-06-11 23:33:09 -0300

    :sparkles: add logic to part one of the project

--- angular-the-complete-guide/the-basics-project/src/app/components/shopping-list/shopping-list.component.ts
+++ angular-the-complete-guide/the-basics-project/src/app/components/shopping-list/shopping-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../common/ingredients/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('any_name', 5),
+    new Ingredient('other_name', 10),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
