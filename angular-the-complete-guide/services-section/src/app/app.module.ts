0b28a0f253878f70d3f9be8a110eb90e3031efcf
André Pereira <andrepereira97@yahoo.com>
2022-06-19 19:30:46 -0300

    :sparkles: add services section project

---
+++ angular-the-complete-guide/services-section/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { AccountComponent } from './components/account/account.component';
+import { NewAccountComponent } from './components/account/new-account/new-account.component';
+
+@NgModule({
+  declarations: [AppComponent, AccountComponent, NewAccountComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
