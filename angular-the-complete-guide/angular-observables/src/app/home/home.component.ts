2148ab99e2d279ff4b6739ce7de66bcae78a7cfa
André Pereira <andrepereira97@yahoo.com>
2022-07-10 20:59:30 -0300

    :sparkles: add operators

--- angular-the-complete-guide/angular-observables/src/app/home/home.component.ts
+++ angular-the-complete-guide/angular-observables/src/app/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription, Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
   private firstObsSubscription: Subscription;
@@ -22,25 +22,34 @@ export class HomeComponent implements OnInit, OnDestroy {
         if (count === 5) {
           observer.complete();
         }
-        if (count > 3) {
-          observer.error(new Error('Count is greater 3!'));
+        if (count > 10) {
+          observer.error(new Error('Count is greater 10!'));
         }
         count++;
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe(
-      (data) => {
-        console.log(data);
-      },
-      (error) => {
-        console.log(error);
-        alert(error.message);
-      },
-      () => {
-        console.log('Completed!');
-      }
-    );
+    this.firstObsSubscription = customIntervalObservable
+      .pipe(
+        filter((data) => {
+          return data > 0;
+        }),
+        map((data: number) => {
+          return 'Round: ' + (data + 1);
+        })
+      )
+      .subscribe(
+        (data) => {
+          console.log(data);
+        },
+        (error) => {
+          console.log(error);
+          alert(error.message);
+        },
+        () => {
+          console.log('Completed!');
+        }
+      );
   }
 
   ngOnDestroy(): void {
