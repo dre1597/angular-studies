ed6995487aa7ed90b15370d3745a4939fdfeec9f
André Pereira <andrepereira97@yahoo.com>
2022-07-10 14:23:21 -0300

    :sparkles: add routing and some cursor:pointer;

--- angular-the-complete-guide/recipe-book-project/src/app/app.module.ts
+++ angular-the-complete-guide/recipe-book-project/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DropdownDirective } from './common/directives/dropdown.directive';
 import { HeaderComponent } from './components/header/header.component';
@@ -24,7 +25,7 @@ import { ShoppingListComponent } from './components/shopping-list/shopping-list.
     ShoppingEditComponent,
     DropdownDirective,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
