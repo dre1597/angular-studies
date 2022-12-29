b910dfaa3323b23a1df2bc78aba9606982ae8797
André Pereira <andrepereira97@yahoo.com>
2022-12-28 22:49:33 -0300

    :sparkles: add login

--- angular-the-complete-guide/recipe-book-project/src/app/components/auth/auth.component.ts
+++ angular-the-complete-guide/recipe-book-project/src/app/components/auth/auth.component.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Observable, Subscription } from 'rxjs';
 
-import { AuthService } from './auth.service';
+import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
 
+  private _subscriptions: Subscription[] = []
+
   constructor(private authService: AuthService) {}
+
+
+  ngOnDestroy(): void {
+    this._subscriptions.forEach((subscription) => subscription.unsubscribe())
+  }
 
   onSwitchMode(): void {
     this.isLoginMode = !this.isLoginMode;
@@ -23,26 +31,31 @@ export class AuthComponent {
       return;
     }
 
+    const email = form.value.email;
+    const password = form.value.password;
+
+    this.isLoading = true;
+
+    let auth$: Observable<AuthResponseData>;
+
     if (this.isLoginMode) {
-      // TODO: login logic
+      auth$ = this.authService.login(email, password);
     } else {
-      const email = form.value.email;
-      const password = form.value.password;
-
-      this.isLoading = true;
-
-      this.authService.signup(email, password).subscribe({
-        next: (resData) => {
-          console.log(resData);
-          this.isLoading = false;
-        },
-        error: (errorMessage) => {
-          this.error = errorMessage;
-          this.isLoading = false;
-        }
-      });
+      auth$ = this.authService.signup(email, password);
     }
 
+    const authSubscription = auth$.subscribe({
+      next: (resData) => {
+        console.log(resData);
+        this.isLoading = false;
+      },
+      error: (errorMessage) => {
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    });
+
+    this._subscriptions.push(authSubscription);
 
     form.reset();
   }
--- angular-the-complete-guide/recipe-book-project/src/app/components/auth/auth.service.ts
+++ angular-the-complete-guide/recipe-book-project/src/app/components/auth/auth.service.ts
@@ -4,12 +4,13 @@ import { catchError, Observable } from 'rxjs';
 
 import { environment } from '../../../environments/environment';
 
-interface AuthResponseData {
+export type AuthResponseData = {
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({
@@ -33,6 +34,36 @@ export class AuthService {
       switch (e.error.error.message) {
         case 'EMAIL_EXISTS':
           errorMessage = 'This email exists already!';
+          break;
+        case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+          errorMessage = 'Too many attempts, try later!';
+          break;
+      }
+
+      throw(errorMessage);
+    }));
+  }
+
+  login(email: string, password: string): Observable<AuthResponseData> {
+    return this.httpClient.post<AuthResponseData>(`${environment.signInUrl}`, {
+      email,
+      password,
+      returnSecureToken: true,
+    }).pipe(catchError((e) => {
+      let errorMessage = 'An unknown error occured!';
+
+      if (!e?.error?.error) {
+        throw errorMessage;
+      }
+
+      switch (e.error.error.message) {
+        case 'EMAIL_NOT_FOUND':
+        case 'INVALID_PASSWORD':
+          errorMessage = 'Incorrect email or password!';
+          break;
+        case 'USER_DISABLED':
+          errorMessage = 'This user has been disabled by an administrador';
+          break;
       }
 
       throw(errorMessage);
