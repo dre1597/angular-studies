eb67fbf214ae641506bdf215548d79f6955f6a83
André Pereira <andrepereira97@yahoo.com>
2023-01-02 23:41:32 -0300

    :recycle: separate the core part into a ng module

--- angular-the-complete-guide/recipe-book-project/src/app/app.module.ts
+++ angular-the-complete-guide/recipe-book-project/src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthInterceptorService } from './components/auth/auth-interceptor.service';
 import { AuthComponent } from './components/auth/auth.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RecipesModule } from './components/recipes/recipes.module';
 import { ShoppingListModule } from './components/shopping-list/shopping-list.module';
+import { CoreModule } from './core-module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -22,17 +22,13 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    ReactiveFormsModule,
     HttpClientModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
-  providers: [{
-    provide: HTTP_INTERCEPTORS,
-    useClass: AuthInterceptorService,
-    multi: true,
-  }],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {
