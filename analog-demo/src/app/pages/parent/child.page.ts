4b6a9026e34bcb90121aeb3df627e0a3c8d7349c
André Pereira <andrepereira97@yahoo.com>
2023-04-29 21:50:55 -0300

    :sparkles: simulates a server call on child page guard example

--- analog-demo/src/app/pages/parent/child.page.ts
+++ analog-demo/src/app/pages/parent/child.page.ts
@@ -3,7 +3,7 @@ import { RouteMeta } from '@analogjs/router';
 
 
 export const routeMeta: RouteMeta = {
-  canActivate: [() => true],
+  canActivate: [() => new Promise(resolve => setTimeout(() => resolve(true), 3000))], //simulates a server call
 };
 
 @Component({
